2a6a854730ac9922ed055fdc491669bcf6702cf8
ltx <lintx1119@gmail.com>
2025-06-04 17:37:29 +0800

    new 新实体类

---
+++ src/main/java/com/hmdp/entity/Test.java
@@ -0,0 +1,17 @@
+package com.hmdp.entity;
+
+/**
+ * @author 林太翔
+ * @date 2025/6/4 17:36
+ */
+public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
